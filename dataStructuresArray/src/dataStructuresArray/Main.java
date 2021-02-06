df2319fa4ad5a0f7205d7e8416054d8f7fe31f80
camzinante <76184417+camzinante@users.noreply.github.com>
2021-02-06 14:36:22 -0600

    Update Main.java

--- dataStructuresArray/src/dataStructuresArray/Main.java
+++ dataStructuresArray/src/dataStructuresArray/Main.java
@@ -6,14 +6,14 @@ public class Main {
 		     int[] Array = {3, 54, 78, 22, 9}; 
 	
 		         int sum = 0;  
-		         int i= 0;  
+		         int i;  
 		        
 		        
 		         for (i = 0; i < Array.length; i++) 
 		         { 
 		            sum += Array[i]; 
 		         }   
-		            
+		            	System.out.println("Hey guys! This is the sum of the Array");
 		         	System.out.print(sum);
 	
 	}
