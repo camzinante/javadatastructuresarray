b949b612abec70dee2dc7e6ff2b0924f4532526f
Cam <Cam@cams-mbp.lan>
2021-02-06 13:44:06 -0600

    full commit upload

---
+++ dataStructuresArray/src/dataStructuresArray/Main.java
@@ -0,0 +1,21 @@
+package dataStructuresArray;
+
+public class Main {
+	public static void main(String[] args) {
+	
+		     int[] Array = {3, 54, 78, 22, 9}; 
+	
+		         int sum = 0;  
+		         int i= 0;  
+		        
+		        
+		         for (i = 0; i < Array.length; i++) 
+		         { 
+		            sum += Array[i]; 
+		         }   
+		            
+		         	System.out.print(sum);
+	
+	}
+
+}
