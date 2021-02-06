039d906243782b5dfbe1484985c240bb70cc2b2b
camzinante <76184417+camzinante@users.noreply.github.com>
2021-02-06 15:02:37 -0600

    Update Main.java

--- dataStructuresArray/src/dataStructuresArray/Main.java
+++ dataStructuresArray/src/dataStructuresArray/Main.java
@@ -15,6 +15,21 @@ public class Main {
 		         }   
 		            	System.out.println("Hey guys! The sum of the array is:");
 		         	System.out.print(sum);
+		
+			double [ ] ArrayAgain = {3, 54, 78, 22, 9};
+		        double maximum = Array[0];
+		        int index = 0;
+		         	
+		         	for (int i1 = 1; i1<ArrayAgain.length; i1++) 
+				{
+		         		if (ArrayAgain[ i1 ] > maximum) 
+					{
+		         			maximum = ArrayAgain [ i1 ];
+		         			index = i1;
+		         		}
+		         	}
+		         // Am I doing something wrong here? I thought when I ran it, I'd get the sum, and then right below it the index? Is it where my system out is positioned? 	
+		         System.out.println(index);
 	
 	}
 
